cb254e7dc1360ed278c969e547a8324fc74456ab
MennoScholten <54530840+MennoScholten@users.noreply.github.com>
2022-09-15 12:00:07 +0200

    Update and rename RTC.c to RTC.h

--- RTC.h
+++ RTC.h
@@ -1,13 +1,22 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+
 volatile uint32_t *RTC_OUTPUT_HIGH 	= (uint32_t*)0x1000004C; // High bits of RTC Counter
 volatile uint32_t *RTC_OUTPUT_LOW 	= (uint32_t*)0x10000048; // Low bits of RTC Counter
 volatile uint32_t *RTC_CONFIG		= (uint32_t*)0x10000040; // Low bits of RTC Counter
 
+void rtc_setup() {
+	*RTC_CONFIG  |= (1 << 12);
+}
+
 int save_rtc_low() {
+
 	int rtc_low_value = *RTC_OUTPUT_LOW;
-	retrun rtc_low_value;
+	return rtc_low_value;
 }
 
 int save_rtc_high() {
 	int rtc_high_value = *RTC_OUTPUT_HIGH;
-	retrun rtc_high_value;
+	return rtc_high_value;
 }
