65e2aae4dbe53119e3a6c112d8e5593a680d384a
Martin Reinok <78325056+martinreinok@users.noreply.github.com>
2022-09-15 15:18:10 +0200

    Microseconds

--- RTC.h
+++ RTC.h
@@ -6,17 +6,29 @@ volatile uint32_t *RTC_OUTPUT_HIGH 	= (uint32_t*)0x1000004C; // High bits of RTC
 volatile uint32_t *RTC_OUTPUT_LOW 	= (uint32_t*)0x10000048; // Low bits of RTC Counter
 volatile uint32_t *RTC_CONFIG		= (uint32_t*)0x10000040; // Low bits of RTC Counter
 
+int clock_frequency = 32768;
+int clock_multiplier_micro = 100000;
+
 void rtc_setup() {
 	*RTC_CONFIG  |= (1 << 12);
 }
 
-int save_rtc_low() {
 
-	int rtc_low_value = *RTC_OUTPUT_LOW;
+uint32_t get_rtc_low() {
+
+	uint32_t rtc_low_value = *RTC_OUTPUT_LOW;
 	return rtc_low_value;
 }
 
-int save_rtc_high() {
-	int rtc_high_value = *RTC_OUTPUT_HIGH;
+uint32_t get_rtc_low_micro() {
+
+	uint32_t rtc_low_value_micro = *RTC_OUTPUT_LOW;
+	return rtc_low_value_micro*clock_multiplier_micro/clock_frequency;
+}
+
+
+
+uint32_t get_rtc_high() {
+	uint32_t rtc_high_value = *RTC_OUTPUT_HIGH;
 	return rtc_high_value;
 }
