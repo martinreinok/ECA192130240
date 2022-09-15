5e338249bc0ce2fa8042b9f5b9179d6634901813
Martin Reinok <martin.reinok@gmail.com>
2022-09-15 11:42:24 +0200

    Added RTC.c fucntion

--- RTC.c
+++ RTC.c
@@ -1,25 +1,13 @@
-/* Copyright 2019 SiFive, Inc */
-/* SPDX-License-Identifier: Apache-2.0 */
-#include <stdint.h>
-#include <stdio.h>
+volatile uint32_t *RTC_OUTPUT_HIGH 	= (uint32_t*)0x1000004C; // High bits of RTC Counter
+volatile uint32_t *RTC_OUTPUT_LOW 	= (uint32_t*)0x10000048; // Low bits of RTC Counter
+volatile uint32_t *RTC_CONFIG		= (uint32_t*)0x10000040; // Low bits of RTC Counter
 
-volatile uint32_t *GPIO_INPUT_VAL 	= (uint32_t*)0x10012000;
-volatile uint32_t *GPIO_INPUT_EN 	= (uint32_t*)0x10012004;
-volatile uint32_t *GPIO_OUTPUT_VAL 	= (uint32_t*)0x1001200C;
-volatile uint32_t *GPIO_OUTPUT_EN 	= (uint32_t*)0x10012008;
-volatile uint32_t *RTC_OUTPUT_HIGH 	= (uint32_t*)0x1001004C; // High bits of RTC Counter
-volatile uint32_t *RTC_OUTPUT_LOW 	= (uint32_t*)0x10010048; // Low bits of RTC Counter
+int save_rtc_low() {
+	int rtc_low_value = *RTC_OUTPUT_LOW;
+	retrun rtc_low_value;
+}
 
-int main() {
-
-	*GPIO_INPUT_EN |= (1 << 9);			// Enable GPIO as input at pin 15 (Echo-Pin)(GPIO 9)
-	*GPIO_INPUT_EN |= (1 << 11);		// Enable GPIO as input at pin 17 (PWM-Servo)(GPIO 11)
-	*GPIO_OUTPUT_EN |= (1 << 1);		// Enable GPIO as output at pin 9 (TRIG-pin)(GPIO 1)
-
-
-
-	while(1){
-		printf("RTC HIGH. %d ", *RTC_OUTPUT_HIGH);
-		printf("\n\rRTC LOW. %d ", *RTC_OUTPUT_LOW);
-	}
+int save_rtc_high() {
+	int rtc_high_value = *RTC_OUTPUT_HIGH;
+	retrun rtc_high_value;
 }
--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
+#include "RTC.c"
 
 //custom write delay function since we do not have one like an Arduino
 void delay(int number_of_microseconds){ //not actually number of seconds
@@ -22,23 +23,20 @@ volatile uint32_t *GPIO_INPUT_VAL 	= (uint32_t*)0x10012000;
 volatile uint32_t *GPIO_INPUT_EN 	= (uint32_t*)0x10012004;
 volatile uint32_t *GPIO_OUTPUT_VAL 	= (uint32_t*)0x1001200C;
 volatile uint32_t *GPIO_OUTPUT_EN 	= (uint32_t*)0x10012008;
-volatile uint32_t *RTC_OUTPUT_HIGH 	= (uint32_t*)0x1000004C; // High bits of RTC Counter
-volatile uint32_t *RTC_OUTPUT_LOW 	= (uint32_t*)0x10000048; // Low bits of RTC Counter
-volatile uint32_t *RTC_CONFIG		= (uint32_t*)0x10000040; // Low bits of RTC Counter
 
 int main() {
-
+	
 	*GPIO_INPUT_EN |= (1 << 9);			// Enable GPIO as input at pin 15 (Echo-Pin)(GPIO 9)
 	*GPIO_INPUT_EN |= (1 << 11);		// Enable GPIO as input at pin 17 (PWM-Servo)(GPIO 11)
 	*GPIO_OUTPUT_EN |= (1 << 1);		// Enable GPIO as output at pin 9 (TRIG-pin)(GPIO 1)
 	*RTC_CONFIG |= (1 << 12);
 
 	while(1){
-		printf("\r RTC HIGH. %d \n", *RTC_OUTPUT_HIGH);
-		printf("\r RTC LOW. %d	\n", *RTC_OUTPUT_LOW);
-	    printf("\r RTC CONFIG. %d \n", *RTC_CONFIG);
+		t1 = save_rtc_low()
 
 		delay(2000);
+		t2 = save_rtc_low()
+		printf("\r RTC DIFFERENCE. %d \n", t2-t1);
 	}
 
 
