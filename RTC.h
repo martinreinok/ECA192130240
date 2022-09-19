04b533a04846b89aa513469f883bc490d2cc3366
MennoScholten <54530840+MennoScholten@users.noreply.github.com>
2022-09-19 11:02:42 +0200

    updated get micro function

--- RTC.h
+++ RTC.h
@@ -12,28 +12,34 @@ const uint64_t clock_multiplier_micro = 1000000;
 const uint64_t clock_multiplier_milli = 1000;
 
 void rtc_setup() {
+	set_bit_low(*RTC_CONFIG, 0);
+	set_bit_low(*RTC_CONFIG, 1);
+	set_bit_low(*RTC_CONFIG, 2);
+
 	set_bit_high(*RTC_CONFIG, 12);
 }
 
 
-uint64_t get_rtc_low() {
-	volatile uint64_t rtc_low_value = *RTC_OUTPUT_LOW;
+uint32_t get_rtc_low() {
+	volatile uint32_t rtc_low_value = *RTC_OUTPUT_LOW;
 	return rtc_low_value;
 }
 
-uint64_t get_rtc_low_micro() {
+uint32_t get_rtc_low_micro() {
 
-	volatile uint64_t rtc_low_value = *RTC_OUTPUT_LOW;
-	return ((rtc_low_value * clock_multiplier_micro)/clock_frequency);
+	volatile uint32_t rtc_low_value = *RTC_OUTPUT_LOW;
+	uint64_t rtc_low_value_Multi = rtc_low_value * clock_multiplier_micro;
+	uint32_t result = rtc_low_value_Multi / clock_frequency;
+	return (result);
 }
 
-uint64_t get_rtc_low_milli() {
+uint32_t get_rtc_low_milli() {
 
-	volatile uint64_t rtc_low_value = *RTC_OUTPUT_LOW;
+	volatile uint32_t rtc_low_value = *RTC_OUTPUT_LOW;
 	return ((rtc_low_value * clock_multiplier_milli)/clock_frequency);
 }
 
-uint64_t get_rtc_high() {
-	volatile uint64_t rtc_high_value = *RTC_OUTPUT_HIGH;
+uint32_t get_rtc_high() {
+	volatile uint32_t rtc_high_value = *RTC_OUTPUT_HIGH;
 	return rtc_high_value;
 }
