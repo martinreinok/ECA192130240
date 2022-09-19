67c678ff529bdc605fd3aad76a49eed0747b4324
Martin Reinok <78325056+martinreinok@users.noreply.github.com>
2022-09-19 19:19:17 +0200

    Update BIT_OPS.h

--- BIT_OPS.h
+++ BIT_OPS.h
@@ -2,13 +2,13 @@
 Bit Operations
 */
 // Return data.bit value
-#define get_bit(data, bit) ( (data>>bit) & 1)
+#define get_bit(data, bit) ( (data>>bit) & 0b1)
 
 // Set data.bit to 1
-#define set_bit_high(data, bit) data |= (1 << bit)
+#define set_bit_high(data, bit) data |= (0b1 << bit)
 
 // Set data.bit to 0
-#define set_bit_low(data, bit)  data &= ~(1 << bit)
+#define set_bit_low(data, bit)  data &= ~(0b1 << bit)
 
 // Return bit range
-#define get_bit_range(data, start_bit, end_bit) ((data >> start_bit) & ((1 << (end_bit - start_bit)) - 1))
+#define get_bit_range(data, start_bit, end_bit) ((data >> start_bit) & ((0b1 << (end_bit - start_bit)) - 0b1))
