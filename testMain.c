395d11528e6d896db70b79cd47b7d931f870aa8a
MennoScholten <54530840+MennoScholten@users.noreply.github.com>
2022-09-19 11:05:41 +0200

    Update testMain.c

--- testMain.c
+++ testMain.c
@@ -2,6 +2,178 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <time.h>
+#include "BIT_OPS.h"
+#include "RTC.h"
+
+/*DATA STRUCT*/
+struct Measurement{
+    int distance;
+    int angle;
+};
+
+
+/*GLOBAL DEFINES*/
+#define ARRAY_MAX_SIZE 65
+
+
+/*GLOBAL VARIABLES*/
+struct Measurement measurements [ARRAY_MAX_SIZE] = {}; // Up to 65 measurements in the array
+int array_element_index = 0;
+int motor_angle = 0;
+const int speed_of_sound = 34300; // cm/s
+const int sensor_timeout = 4000;  // Units ?
+
+
+/*PIN DECLARATIONS*/
+volatile uint32_t *GPIO_INPUT_VAL 	= (uint32_t*)0x10012000;
+volatile uint32_t *GPIO_INPUT_EN 	= (uint32_t*)0x10012004;
+volatile uint32_t *GPIO_OUTPUT_VAL 	= (uint32_t*)0x1001200C;
+volatile uint32_t *GPIO_OUTPUT_EN 	= (uint32_t*)0x10012008;
+
+/*
+
+//custom write delay function since we do not have one like an Arduino
+void delay(int number_of_microseconds){ //not actually number of seconds
+
+// Converting time into multiples of a hundred nS
+int hundred_ns = 10 * number_of_microseconds;
+
+// Storing start time
+clock_t start_time = clock();
+
+// looping till required time is not achieved
+while (clock() < start_time + hundred_ns);
+
+}
+*/
+
+/*FUNCTION DECLARATIONS*/
+void save_measurement(int time, int angle);
+void stream_output(int distance, int angle);
+void bubble_sort(struct Measurement *array, int array_size);
+
+
+int main() {
+	set_bit_high(*GPIO_INPUT_EN, 9); 	// Enable GPIO as input at pin 15 (Echo-Pin)(GPIO 9)
+	//set_bit_high(*GPIO_INPUT_EN, 11); 	// Enable GPIO as input at pin 17 (PWM-Servo)(GPIO 11)
+	set_bit_high(*GPIO_OUTPUT_EN, 1);	// Enable GPIO as output at pin 9 (TRIG-pin)(GPIO 1)
+
+	rtc_setup();
+	// Initialize servo
+	// Move servo to starting position 1 / 65
+
+	while(1) {
+		bool timeout = false;
+		int waitTime = get_rtc_low_micro() + 10;
+		// Trigger Ultrasound sensor
+		set_bit_high(*GPIO_OUTPUT_VAL, 1); 	// Sets pin 1 HIGH
+		while(get_rtc_low_micro() <= waitTime);
+		set_bit_low(*GPIO_OUTPUT_VAL, 1);	// Sets pin 1 LOW
+
+		// Get starting time
+		int start_time = get_rtc_low_micro();
+		printf("\r Start us: %u\n", get_rtc_low_micro());
+		// Wait for sensor signal or timeout
+		while (timeout == false){
+			int echoIn = (*GPIO_INPUT_VAL >> 9) & 0b1;
+			if (get_rtc_low_micro() >= start_time + sensor_timeout){
+				//Signal timeout
+				printf("\r TimeOut \n");
+				timeout = true;
+				break;
+			}
+
+		// Only save value if sensor did not timeout
+			if (echoIn >= 1){
+				int end_time = get_rtc_low_micro();
+				printf("\r Stop us: %u\n", get_rtc_low_micro());
+				printf("\r Received signal: %d, %d\n", end_time-start_time, motor_angle);
+				save_measurement(end_time-start_time, motor_angle);
+				echoIn = 0;
+				break;
+			}
+
+		// Print results in terminal
+		// stream_output(int distance, int angle);
+
+
+		// Move motor to next position here
+
+		// If measurements are over
+		/*
+
+		Print all array distances:
+		for(int i=0; i<array_element_index; i++){
+            	printf("Array distance[%d]: %d\n", i, measurements[i].distance);
+        	}
+
+		Sort the array:
+		bubble_sort(measurements, array_element_index);
+
+
+		*/
+		}
+	}
+}
+
+
+
+/*FUNCTIONS*/
+void save_measurement(int time, int angle){
+    // uint32_t distance = (speed_of_sound * time)/2;
+    measurements[array_element_index].distance = time;
+    measurements[array_element_index].angle = angle;
+    array_element_index++;
+}
+
+
+void stream_output(int distance, int angle){
+    printf("\r Stream distance results\n");
+}
+
+
+void bubble_sort(struct Measurement *array, int array_size) {
+
+    for(int i = 0; i<array_size; i++) {
+        int swaps = 0;         //flag to detect any swap is there or not
+        for(int j = 0; j<array_size-i-1; j++) {
+            if(array[j].distance > array[j+1].distance) {       //when the current item is bigger than next
+                // Swap distance
+                //printf("Swap %d with %d\n", array[j].distance, array[j+1].distance);
+                int temp;
+                temp = array[j].distance;
+                array[j].distance = array[j+1].distance;
+                array[j+1].distance = temp;
+
+                // Swap angle
+                temp = array[j].angle;
+                array[j].angle = array[j+1].angle;
+                array[j+1].angle = temp;
+
+                swaps = 1;    //set swap flag
+            }
+        }
+
+        if(!swaps){
+            break; // No swap in this pass, so array is sorted
+        }
+    }
+    // Print result
+    for(int i=0; i<array_size; i++){
+        printf("\r Array distance sorted[%d]: %d\n", i, array[i].distance);
+        printf("\r Array angle sorted[%d]: %d\n", i, array[i].angle);
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// OLD PROGRAM
+////////////////////////////////////////////////////////////////////////////////////////////
+/*
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
 
 #include "RTC.h"
 
@@ -18,28 +190,33 @@ int main() {
 
 	rtc_setup();
 
+	uint64_t startTime = 0;
+	uint64_t stopTime = 0;
+
 	while(1){
 
-		//*GPIO_OUTPUT_VAL |= (1 << 1);		// Sets GPIO 1 HIGH
-		//startTime = get_rtc_low_micro();
+		*GPIO_OUTPUT_VAL |= (1 << 1);		// Sets GPIO 1 HIGH
+		startTime = get_rtc_low_micro();
 		//printf("\r start Time: %u.\n ",startTime);
+		//printf("\r Reg: %u. sttime ms: %u\n ",*RTC_OUTPUT_LOW, startTime);
 
-		printf("\r Reg: %u. ms: %u\n ",*RTC_OUTPUT_LOW, get_rtc_low_milli());
-		printf("\r Reg: %u. us: %u\n ",*RTC_OUTPUT_LOW, get_rtc_low_micro());
-		/*
+		//printf("\r Reg: %u. ms: %u\n ",*RTC_OUTPUT_LOW, get_rtc_low_milli());
+		//printf("\r Reg: %u. us: %u\n ",*RTC_OUTPUT_LOW, get_rtc_low_micro());
+
 		uint64_t tDelay = get_rtc_low_milli() + 10;
 		while(!(get_rtc_low_milli() >= tDelay));
-
 		*GPIO_OUTPUT_VAL &= ~(1 << 1);		// Sets GPIO 1 LOW
 		uint64_t tWait = get_rtc_low_milli();
-		while(!((*GPIO_INPUT_VAL >> 9) & 0b1) & ((get_rtc_low_milli() - tWait) <= 10000));
+		while(!((*GPIO_INPUT_VAL >> 9) & 0b1) & ((get_rtc_low_milli() - tWait) <= 1000));
 		stopTime = get_rtc_low_micro();
-		printf("\r stop Time: %u.\n ", stopTime);
-		printf("\r Stop Time Real: %u.\n ",get_rtc_low_micro());
+		//printf("\r Reg: %u. stop Time: %u.\n ",*RTC_OUTPUT_LOW,  stopTime);
+		//printf("\r Reg: %u. Stop Time Real: %u.\n ",*RTC_OUTPUT_LOW, get_rtc_low_micro());
 		uint64_t timeDiff = stopTime - startTime;
-		printf("\r TimeDiff: %u.\n ", timeDiff);
-		*/
-		/*
+		printf("\r Reg: %u. Start: %u\n ",*RTC_OUTPUT_LOW, startTime);
+		printf("\r Reg: %u. Stop: %u\n ",*RTC_OUTPUT_LOW, stopTime);
+		printf("\r Reg: %u. TimeDiff: %u.\n ",*RTC_OUTPUT_LOW,  timeDiff);
+		while((*GPIO_INPUT_VAL >> 9) & 0b1);
+
 		if(((*GPIO_INPUT_VAL >> 9) & 0b1)){
 			stopTime = get_rtc_low_micro();
 			uint64_t timeDiff = stopTime - startTime;
@@ -48,20 +225,22 @@ int main() {
 		else {
 			printf("\r No Time");
 		}
-		*/
+
 
 
 
 		//while(!((*GPIO_INPUT_VAL >> 9) & 0b1) & ((save_rtc_low() - timeStart) <= 100000));
-		/*
+
 		if(((*GPIO_INPUT_VAL >> 9) & 0b1)){
 			int timeStop = save_rtc_low();
 			int timeDiff = timeStop - timeStart;
 			int length = timeDiff/58;
 			printf("\r TimeStop: %d. TimeStart: %d. TimeDiff: %d. Got distance: %d. \n ", timeStop, timeStart, timeDiff, length);
 		}
-		*/
+
 
 
 	}
 }
+
+*/
