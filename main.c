0c06f0aee8880f4af101fef38c08a59713ab12a5
MennoScholten <54530840+MennoScholten@users.noreply.github.com>
2022-09-21 17:28:49 +0200

    Assignment 1 Final version (Single File)

--- main.c
+++ main.c
@@ -1,11 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <time.h>
-#include "BIT_OPS.h"
-#include "RTC.h"
 #include <math.h>
-
 
 /*DATA STRUCT*/
 struct Measurement{
@@ -13,17 +9,20 @@ struct Measurement{
     int angle;
 };
 
-
 /*PIN DEFINES*/
 #define TRIG_PIN 1		// Trigger pin at GPIO 1 (pin 9) (OUTPUT)
 #define ECHO_PIN 9		// Echo pin at GPIO 9 (pin 15) (INPUT)
 #define SERVO_PIN 11	// Servo pin at GPIO 11 (pin 17) (OUTPUT)
 
-
 /*FUNCTION DECLARATIONS*/
 void setInputPin(uint32_t pinNumber);
 void setOutputPin(uint32_t pinNumber);
 void setPWMPin(uint32_t pinNumber);
+void rtc_setup();
+uint32_t get_rtc_low();
+uint32_t get_rtc_low_micro();
+uint32_t get_rtc_low_milli();
+uint32_t get_rtc_high();
 void initSensor();
 void setPWM1(uint32_t frequency, uint32_t dutyCycle);
 void setDutyCycle1(uint32_t dutyCycle);
@@ -39,15 +38,14 @@ uint32_t read_echo();
 float calcMotorAngle(uint32_t curposition, bool curDirection);
 uint32_t calcNewPosition(uint32_t curposition, bool curDirection);
 
-
-/*Global VARIABLES*/
+/*Global DECLARATIONS/VARIABLES*/
 #define MeasurementAmount 65
 #define sweepAmount 3
 #define amountClosest 3
 #define ServoPWMFreq 200
 #define minDutyCycle 10000
 #define maxDutyCycle 37950
-#define maxServoAngle 120
+#define maxServoAngle 130
 #define triggerDutyCycle 50000
 #define triggerPWMFreq 20000
 static uint32_t BASE_FREQUENCY = 16000000;
@@ -56,6 +54,9 @@ struct Measurement closestMeasurements[amountClosest] = {}; // 3 Closest positio
 static uint32_t speed_of_sound = 343; // m/s
 static uint32_t sensor_timeout = 25000;  // Units ?
 static uint32_t loopTime = 60000;
+static uint64_t clock_frequency = 32768;
+static uint64_t clock_multiplier_micro = 1000000;
+static uint64_t clock_multiplier_milli = 1000;
 bool sweepDirection = false;		// Direction of sweep
 
 /*REGISTRY DECLARATIONS*/
@@ -66,6 +67,10 @@ volatile uint32_t *GPIO_OUTPUT_EN 	= (uint32_t*)0x10012008;		// Registry for OUT
 volatile uint32_t *GPIO_PUE 		= (uint32_t*)0x10012010;		// Registry for INPUT Internal PULL UP ENABLE
 volatile uint32_t *IOF_EN 			= (uint32_t*)0x10012038;		// Registry for HW-Driven funtions (IOF)
 volatile uint32_t *IOF_SEL 			= (uint32_t*)0x1001203C;		// Registry for HW-Driven funtions (IOF)
+
+volatile uint32_t *RTC_OUTPUT_HIGH 	= (uint32_t*)0x1000004C; 		// High bits of RTC Counter
+volatile uint32_t *RTC_OUTPUT_LOW 	= (uint32_t*)0x10000048; 		// Low bits of RTC Counter
+volatile uint32_t *RTC_CONFIG		= (uint32_t*)0x10000040; 		// Low bits of RTC Counter
 
 volatile uint32_t *PWM0_1_CONFIG	= (uint32_t*)0x10015000;		// Registry for PWM CONFIG TRIGGER PIN
 volatile uint32_t *PWM0_1_COUNT		= (uint32_t*)0x10015008;		// Registry for PWM Counter TRIGGER PIN
@@ -83,6 +88,7 @@ volatile uint32_t *PWM2_1_CMP3		= (uint32_t*)0x1003502C;		// Registry for PWM CO
 
 
 int main() {
+
 	setInputPin(ECHO_PIN);
 
 	// Initialize One shot pwm at Trigger pin
@@ -95,19 +101,15 @@ int main() {
 
 	initSensor(); // Stabilizes the sensor output
 
-	uint32_t sweep;
+	for(uint32_t sweep = 1; sweep <= sweepAmount; sweep ++){
 
-	for(sweep = 1; sweep <= sweepAmount; sweep ++){
-		uint32_t measurement;
-
-		for(measurement = 0; measurement <= MeasurementAmount; measurement ++){
+		for(uint32_t measurement = 0; measurement <= MeasurementAmount; measurement ++){
 			uint32_t motor_angle = calcMotorAngle(measurement, sweepDirection);
 
 			uint32_t distance = 0;
 
 			// Take an average of 3 measurements
-			uint32_t i;
-			for(i = 1; i <= 3; i ++){
+			for(uint32_t i = 1; i <= 3; i ++){
 				uint32_t minLoopTime = get_rtc_low_micro() + loopTime;	// Minimum time for loop to complete
 				// Trigger sensor
 				resetSingleShot();
@@ -141,12 +143,12 @@ int main() {
 
 			setDutyCycle2(calcNewPosition(measurement, sweepDirection));
 		}
+
 		sweepDirection = !sweepDirection;
 
 	}
 
 	streamMeasurements();
-
 
 	/*
 	// Code for testing the sensors accuracy
@@ -182,26 +184,6 @@ int main() {
 		setDutyCycle2(maxDutyCycle);	// Maximum position of rotation.
 	}
 	*/
-
-
-
-	// Print results in terminal
-	// stream_output(int distance, int angle);
-
-	// Move motor to next position here
-
-	// If measurements are over
-
-
-	/*
-	Print all array distances:
-	for(int i=0; i<array_element_index; i++){
-			printf("Array distance[%d]: %d\n", i, measurements[i].distance);
-		}
-	Sort the array:
-	bubble_sort(measurements, array_element_index);
-	*/
-
 }
 
 void setInputPin(uint32_t pinNumber){
@@ -228,6 +210,41 @@ void setPWMPin(uint32_t pinNumber){
 	*IOF_SEL		|= (1<<pinNumber);
 }
 
+void rtc_setup(){
+	*RTC_CONFIG &= ~(0b1<<0);
+	*RTC_CONFIG &= ~(0b1<<1);
+	*RTC_CONFIG &= ~(0b1<<2);
+
+	*RTC_CONFIG |= (0b1<<12);
+	printf("\r RTCregister: %u \n", *RTC_CONFIG);
+}
+
+
+uint32_t get_rtc_low(){
+	volatile uint32_t rtc_low_value = *RTC_OUTPUT_LOW;
+	return rtc_low_value;
+}
+
+uint32_t get_rtc_low_micro(){
+
+	volatile uint32_t rtc_low_value = *RTC_OUTPUT_LOW;
+	uint64_t rtc_low_value_Multi = rtc_low_value * clock_multiplier_micro;
+	uint32_t result = rtc_low_value_Multi / clock_frequency;
+	return (result);
+}
+
+uint32_t get_rtc_low_milli(){
+
+	volatile uint32_t rtc_low_value = *RTC_OUTPUT_LOW;
+	return ((rtc_low_value * clock_multiplier_milli)/clock_frequency);
+}
+
+uint32_t get_rtc_high(){
+	volatile uint32_t rtc_high_value = *RTC_OUTPUT_HIGH;
+	return rtc_high_value;
+}
+
+
 void initSensor() {
 	resetSingleShot();
 	uint32_t waitTime = get_rtc_low_micro() + 60000;
@@ -245,28 +262,16 @@ void setDutyCycle1(uint32_t dutyCycle){
 
 void setPWM1(uint32_t frequency, uint32_t dutyCycle){
 	int maxComparator =  256 - 1; // Maximum 8-bit comparator value
-	uint32_t comparator; //= BASE_FREQUENCY / (frequency * pow(2,scale));
-	int scale = -1;
+	int scale = 0;
+	uint32_t comparator = (BASE_FREQUENCY) / (frequency * pow(2,scale));
 
-	do
-	{
-		scale++;
+	while(comparator > maxComparator){
+		scale += 1;
 		comparator = (BASE_FREQUENCY) / (frequency * pow(2,scale));
 		printf("\r PWM1 Comparator: %u\n", comparator);
 		printf("\r PWM1 SCALE: %u\n", scale);
-	} while (comparator > maxComparator);
-	/*
-	while(comparator > maxComparator){
-		scale += 1;
-		comparator = BASE_FREQUENCY / (frequency * pow(2,scale));
-		if(comparator <= maxComparator) {
-			scale -= 1;
-			printf("\r SCALE Break: %u\n", scale);
-			break;
-		}
-		printf("\r SCALE: %u\n", scale);
 	}
-	*/
+
 	*PWM0_1_CMP0 = 0b0000000000000000;
 	*PWM0_1_CMP0 |= comparator;
 	*PWM0_1_CONFIG |= scale;
@@ -284,28 +289,16 @@ void setDutyCycle2(uint32_t dutyCycle){
 
 void setPWM2(uint32_t frequency, uint32_t dutyCycle){
 	int maxComparator =  65536 - 1; // Maximum 16-bit comparator value
-	uint32_t comparator; //= BASE_FREQUENCY / (frequency * pow(2,scale));
-	int scale = -1;
+	int scale = 0;
+	uint32_t comparator = (BASE_FREQUENCY) / (frequency * pow(2,scale));
 
-	do
-	{
-		scale++;
+	while(comparator > maxComparator){
+		scale ++;
 		comparator = (BASE_FREQUENCY) / (frequency * pow(2,scale));
 		printf("\r PWM2 Comparator: %u\n", comparator);
 		printf("\r PWM2 SCALE: %u\n", scale);
-	} while (comparator > maxComparator);
-	/*
-	while(comparator > maxComparator){
-		scale += 1;
-		comparator = BASE_FREQUENCY / (frequency * pow(2,scale));
-		if(comparator <= maxComparator) {
-			scale -= 1;
-			printf("\r SCALE Break: %u\n", scale);
-			break;
-		}
-		printf("\r SCALE: %u\n", scale);
 	}
-	*/
+
 	*PWM2_1_CMP0 = 0b0000000000000000;
 	*PWM2_1_CMP0 |= comparator;
 	*PWM2_1_CONFIG |= scale;
@@ -369,11 +362,12 @@ void save_measurement(int distance, int angle, uint32_t measurement){
 }
 
 void streamMeasurements(){
+	printf("\r \n");
 	printf("\r All the closest measurements: \n");
 	for(int i = 0; i < MeasurementAmount; i++){
-		printf("\r Measurement: %u \n", i);
+		printf("\r Measurement: %u \n", i + 1);
 		printf("\r Distance: %d \n",closestPositions[i].distance);
-		printf("\r Angle: (%f)\n", closestPositions[i].angle);
+		printf("\r Angle: (%u)\n", closestPositions[i].angle);
 		printf("\r \n");
 	}
 
@@ -423,22 +417,20 @@ uint32_t calcNewPosition(uint32_t curposition, bool curDirection){
 	if(curDirection == false){
 		if(curposition < MeasurementAmount){
 			newPosition = (curposition + 1) * deltaMeasurement + minDutyCycle;
-			return(newPosition);
 		}
 		else {
 			newPosition = (curposition) * deltaMeasurement + minDutyCycle;
-			return(newPosition);
 		}
 	}
 
 	else if(curDirection == true) {
 		if(curposition < MeasurementAmount){
 			newPosition = (MeasurementAmount - curposition) * deltaMeasurement + minDutyCycle;
-			return(newPosition);
 		}
 		else {
 			newPosition = minDutyCycle;
-			return(newPosition);
 		}
 	}
+
+	return(newPosition);
 }
